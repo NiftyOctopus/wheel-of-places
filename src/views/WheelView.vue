31f3ba5d71c2d33a6e5fd0601285cd4de6f44d85
Bryan <bgchambers@pm.me>
2024-08-03 12:55:42 -0400

    Added a simple Wheel view with geocode api call

---
+++ src/views/WheelView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Welcome to the Wheel View!</h1>
+    <p>This is a simple Vue component.</p>
+    <button @click="geocode">Geocode!</button>
+    <input type="text" v-model="address" placeholder="Enter address">
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import { ref } from 'vue'
+
+const address = ref('')
+
+const geocode = async () => {
+  console.log('Geocoding...')
+  const response = await fetch(`https://wheel-of-proxy.niftyoctopus.workers.dev/geolocation?address=${encodeURIComponent(address.value)}`)
+  const data = await response.json()
+  console.log(data.results[0].geometry.location)
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
