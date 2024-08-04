bb44ebebe349b064b846e5c627fdc053753d81fa
Bryan <bgchambers@pm.me>
2024-08-04 19:27:17 -0400

    Added vue3-roulette wheel component

--- src/views/WheelView.vue
+++ src/views/WheelView.vue
@@ -4,12 +4,14 @@
     <p>This is a simple Vue component.</p>
     <button @click="geocode">Geocode!</button>
     <input type="text" v-model="address" placeholder="Enter address">
+    <Roulette ref="wheel" :items="items" @click="launchWheel" />
   </div>
 </template>
 
 <script setup lang="ts">
 
 import { ref } from 'vue'
+import { Roulette } from 'vue3-roulette'
 
 const address = ref('')
 
@@ -26,6 +28,18 @@ const geocode = async () => {
 // The request format should be for the google nearby places API (new)
 
 
+const wheel = ref(null);
+
+const items = [
+  { id: 1, name: "Banana", htmlContent: "Banana", textColor: "black", background: "" },
+  { id: 2, name: "Apple", htmlContent: "Apple", textColor: "black", background: "" },
+  { id: 3, name: "Orange", htmlContent: "Orange", textColor: "black", background: "" },
+  { id: 4, name: "Cherry", htmlContent: "Cherry", textColor: "black", background: "" },
+];
+
+function launchWheel (){
+  wheel.value.launchWheel();
+}
 
 </script>
 
